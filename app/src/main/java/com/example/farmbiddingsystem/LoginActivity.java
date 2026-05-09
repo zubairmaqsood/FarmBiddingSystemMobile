0202edc68e7bd3c173d175663d9548ffc9f54f09
zubairmaqsood <149279927+zubairmaqsood@users.noreply.github.com>
2026-05-09 16:10:04 +0500

    add material design elements
    add material design elements to form and remove unnecassary elements

--- app/src/main/java/com/example/farmbiddingsystem/LoginActivity.java
+++ app/src/main/java/com/example/farmbiddingsystem/LoginActivity.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class login extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
