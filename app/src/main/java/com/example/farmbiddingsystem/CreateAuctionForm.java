9229a6cac5123da2d2f64a79f257382a3ae3df00
zubairmaqsood <149279927+zubairmaqsood@users.noreply.github.com>
2026-06-04 00:50:59 +0500

    fix scrolling issue
    fix UI scrolling of edit profile page and auction form page

--- app/src/main/java/com/example/farmbiddingsystem/CreateAuctionForm.java
+++ app/src/main/java/com/example/farmbiddingsystem/CreateAuctionForm.java
@@ -66,14 +66,7 @@ public class CreateAuctionForm extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_create_auction_form);
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         // Find Views
         backBtn = findViewById(R.id.backBtn);
